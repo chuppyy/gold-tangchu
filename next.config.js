cd8bcc34345f59f1ba78b9002b7d9805f145ed0b
i am Chuppyy <62337417+chuppyy@users.noreply.github.com>
2023-12-18 10:25:43 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ module.exports = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "api.sportsandtravelonline.com",
+        hostname: "api.goldstarassets.com",
         port: "",
         pathname: "/**",
       },
