1c7681da8fa97966c88cfd95ccfab716a6325685
i am Chuppyy <62337417+chuppyy@users.noreply.github.com>
2024-02-07 22:57:41 +0700

    Update [slug].tsx

--- src/pages/[slug].tsx
+++ src/pages/[slug].tsx
@@ -43,15 +43,7 @@ export default function Page(data: any) {
         <Script src="/qcscript.js" />
         <div className="container-flu details">
          
-       <ins
-      className="adsbygoogle"    
-     style={{ display: 'block' }}
-     data-ad-client="ca-pub-3619133031508264"
-     data-ad-slot="1236418798"
-     data-ad-format="auto"
-     data-full-width-responsive="true"
-    />    
-          
+     
           <h1>{article.name}</h1>     
   <ins
       className="adsbygoogle"    
