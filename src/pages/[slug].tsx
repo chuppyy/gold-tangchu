23ed63bd13b7b890cb317fd4c60b3741d9bf876a
i am Chuppyy <62337417+chuppyy@users.noreply.github.com>
2024-03-15 22:07:44 +0700

    Update [slug].tsx

--- src/pages/[slug].tsx
+++ src/pages/[slug].tsx
@@ -137,6 +137,30 @@ export default function Page(data: any) {
     } catch (err) {
       console.log("err2222");
     }
+   // get all iframe
+    const iframes = document.querySelectorAll("iframe");
+    iframes.forEach((iframe: HTMLIFrameElement) => {
+      if (iframe) {
+        if (iframe.src.includes("twitter")) {
+          iframe.style.height = window.innerWidth <= 525 ? "650px" : "827px";
+          iframe.style.display = "block";
+          iframe.style.width = window.innerWidth <= 525 ? "100%" : "550px";
+          iframe.style.margin = "0 auto";
+        }else if (iframe.src.includes("instagram")) {
+          iframe.style.height = window.innerWidth <= 525 ? "553px" : `${628}px`;
+          iframe.style.display = "block";
+          iframe.style.width = window.innerWidth <= 525 ? "100%" : "100%";
+          iframe.style.margin = "0 auto";
+        }else{    
+               iframe.style.height = window.innerWidth <= 525 ? "250px" : "300px";
+          iframe.style.display = "block";
+          iframe.style.width = window.innerWidth <= 525 ? "100%" : "100%";
+          iframe.style.margin = "0 auto";
+        }
+      }
+    });
+
+    
   }, []);
   return (
     <>
